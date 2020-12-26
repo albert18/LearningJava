7c3b743988dd430b167fe4d029b7bd662cb3a69a
albert18 <erertech@gmail.com>
2020-12-26 08:39:56 -0500

    Added information

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a test again");
+        //System.out.println("This is a test again");
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        //char studentFirstInitial = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+        String studentName = "Kayla";
+        char studentFirstInitial = studentName.charAt(0);
+
+
+//        System.out.println(studentAge);
+        System.out.println(studentName + " " +  studentAge);
+        System.out.println(studentFirstInitial);
+
+
     }
 }
